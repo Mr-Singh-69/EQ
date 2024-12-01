f7b6f34b6120ac06cb178c122a4d9c0bcf31ef9d
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-12-01 14:40:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+
+client.commands = new Collection();
+
+
+const { loadCommands } = require('./handlers/commandHandler');
+loadCommands(client);
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error executing this command!', ephemeral: true });
+    }
+});
+
+
+client.login(process.env.TOKEN);
