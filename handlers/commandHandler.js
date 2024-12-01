506da25c68299c156e87acf186024a0f61e3c134
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-12-01 14:48:30 +0530

    Update commandHandler.js

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+function loadCommands(client) {
+    const commandsPath = path.join(__dirname, '../commands');
+    const commandFolders = fs.readdirSync(commandsPath);
+
+    for (const folder of commandFolders) {
+        const folderPath = path.join(commandsPath, folder);
+        const commandFiles = fs.readdirSync(folderPath).filter((file) => file.endsWith('.js'));
+
+        for (const file of commandFiles) {
+            const command = require(path.join(folderPath, file));
+            if (command.name && command.execute) {
+                client.commands.set(command.name, command);
+            } else {
+                console.error(`Command at ${file} is missing "name" or "execute" property.`);
+            }
+        }
+    }
+}
+
+module.exports = { loadCommands };
 
