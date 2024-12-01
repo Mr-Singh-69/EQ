15eab123982e1c020d0495beb6fbf6f3cf924565
S.Navdeep Singh <navdeep13dps@gmail.com>
2024-12-01 14:50:28 +0530

    Update joke.js

--- commands/fun/joke.js
+++ commands/fun/joke.js
@@ -1,15 +1,12 @@
-const { SlashCommandBuilder } = require('discord.js');
-
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('joke')
-        .setDescription('Tells a random joke'),
-    async execute(interaction) {
+    name: 'joke',
+    description: 'Tells a random joke',
+    execute(message, args) {
         const jokes = [
             "Why don’t skeletons fight each other? They don’t have the guts.",
             "Why did the scarecrow win an award? Because he was outstanding in his field!",
         ];
         const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
-        await interaction.reply(randomJoke);
+        message.reply(randomJoke);
     },
 };
